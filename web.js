ad1d0cecd9e7218106d793691496db8512988296
Mari Muraki <marimuraki@gmail.com>
2013-07-21 03:52:23 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World! Hello World!');
+  var fs = require('fs');
+  var text = new Buffer(fs.readFileSync('index.html'),'utf-8');
+  response.send('Hello!')
 });
 
 var port = process.env.PORT || 5000;
