56346b4086fe63762006a90804f6b27d9ab658d1
Mari Muraki <marimuraki@gmail.com>
2013-07-22 18:48:18 +0000

    updated grader.js with corrections

--- grader.js
+++ grader.js
@@ -7,8 +7,8 @@ Uses commander.js and cheerio. Teaches command line application development and 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html");
-var CHECKSFILE_DEFAULT = "checks.json");
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -45,7 +45,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program 
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>' 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
