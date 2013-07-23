c62e231495944d9cc066096a29348fe1d27c3ec2
Mari Muraki <marimuraki@gmail.com>
2013-07-23 07:37:54 +0000

    updated grader.js to take URLs

--- grader.js
+++ grader.js
@@ -4,11 +4,14 @@
 Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
 */
 
+var sys = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "https://spark-public.s3.amazonaws.com/startup/code/bitstarter.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,18 +41,47 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+var checkHtmlUrl = function(url, checksfile, callback) {
+    rest.get(url).on('complete', function(result) {
+        // sys.puts(result);
+        if (result instanceof Error) {
+            sys.puts('Error: ' + result.message);
+            this.retry(5000);
+        } else {
+            $ = cheerio.load(result);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+            for(var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
+        }
+        callback(out);
+    })
+};
+
+
 var clone = function(fn) {
     return fn.bind({});
 };
 
 if(require.main == module) {
-    program 
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url')
+        .parse(process.argv);
+    if(program.url) {
+        checkHtmlUrl(program.url, program.checks, function(result) {
+            var outJson = JSON.stringify(result, null, 4);
+            console.log(outJson);
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+	}
 }
 else {
     exports.checkHtmlFile = checkHtmlFile;
